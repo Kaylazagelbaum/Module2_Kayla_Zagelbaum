347e08889505e7e26342d91fdb4c8dcc3194171c
Kaylazagelbaum <kzagelba@student.touro.edu>
2025-09-12 14:05:29 -0400

    Update minutes_to_hours_and_minutes.py

--- minutes_to_hours_and_minutes.py
+++ minutes_to_hours_and_minutes.py
@@ -12,12 +12,15 @@ Pseudocode:
         D. Tell my user how many hours and minutes it is
 
 """
-def minutes_to_hours_and_minutes():    #this names the function
+ #this names the function
+def minutes_to_hours_and_minutes():   
     question = "How many minutes"    #this asks the user how many minutes
     answer = int(input(question))    #this converts the answer from a string into and integer and labels it answer
     hours = answer // 60    #this divides the answer by 60 and saves it as hours
     minutes = answer % 60    #this divides the answer by 60, provides the remainder, and saves the remainder as minutes
-    print("That is equivalent to", hours, "hours and", minutes, "minutes") #this prints the answer that the user will see
+     #this prints the answer that the user will see
+    print("That is equivalent to", hours, "hours and", minutes, "minutes") 
 
-minutes_to_hours_and_minutes()    #this calls the function so that it will be executed
+#this calls the function so that it will be executed
+minutes_to_hours_and_minutes()    
           

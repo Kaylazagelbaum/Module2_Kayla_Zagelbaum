c6ea57df4424af5a620364d478d7137ffe6e1a54
Kaylazagelbaum <kzagelba@student.touro.edu>
2025-09-21 19:20:30 -0400

    Add files via upload

---
+++ Kayla_Zagelbaum_campus_cafe_sample.py
@@ -0,0 +1,22 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+==== RESTART: /Users/kaylazagelbaum/Documents/KaylaZagelbaum_campus_cafe.py ====
+============================== 
+ Welcome to Campus Cafe! 
+ ===Menu=== 
+ Coffee = $2.25 
+ Muffin = $2.75 
+ ==============================
+how many coffees?3
+how many muffins?2
+enter tip percent(e.g. 10 for 10%)20
+------------------------------- 
+ ---Receipt--- 
+ 3 x Coffee @ $2.25 = $6.75 
+ 2 x Muffin @ $2.75= $5.50 
+ Tax(8.875%)= $1.09 
+ Tip= $2.45 
+ TOTAL= $15.79 
+ Thank you for your business! 
+ ------------------------------
